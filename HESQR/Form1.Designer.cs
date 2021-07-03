80c8ea13e9243f91373f7872e0c59f04b5b979da
MalikMelih <melihhoran@hotmail.com>
2021-07-04 00:14:43 +0300

    17/06/2021 - V.1.0.0 Beta
    V.1.0.0 Beta First Build

---
+++ HESQR/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace HESQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_hes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pict = new System.Windows.Forms.PictureBox();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(339, 216);
+            this.btn_ok.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(194, 34);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "QR Kod Oluştur";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(339, 111);
+            this.txt_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(212, 23);
+            this.txt_name.TabIndex = 1;
+            // 
+            // txt_hes
+            // 
+            this.txt_hes.Location = new System.Drawing.Point(339, 164);
+            this.txt_hes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_hes.Name = "txt_hes";
+            this.txt_hes.Size = new System.Drawing.Size(212, 23);
+            this.txt_hes.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "HES Kodu";
+            // 
+            // pict
+            // 
+            this.pict.Location = new System.Drawing.Point(10, 25);
+            this.pict.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pict.Name = "pict";
+            this.pict.Size = new System.Drawing.Size(205, 166);
+            this.pict.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pict.TabIndex = 5;
+            this.pict.TabStop = false;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(599, 279);
+            this.Controls.Add(this.pict);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_hes);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.btn_ok);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_hes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pict;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
