82d5d7ea6be9077517f4b3485f2cdc625cf2d075
MalikMelih <melihhoran@hotmail.com>
2021-07-04 00:20:56 +0300

    18/06/2021 - V.1.0.0
    V.1.0.0 Release Build

---
+++ HesCodeQr/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace HesCodeQr
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pict = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_hes = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pict
+            // 
+            this.pict.Location = new System.Drawing.Point(39, 44);
+            this.pict.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pict.Name = "pict";
+            this.pict.Size = new System.Drawing.Size(205, 166);
+            this.pict.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pict.TabIndex = 11;
+            this.pict.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "HES Kodu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // txt_hes
+            // 
+            this.txt_hes.Location = new System.Drawing.Point(352, 173);
+            this.txt_hes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_hes.Name = "txt_hes";
+            this.txt_hes.Size = new System.Drawing.Size(212, 23);
+            this.txt_hes.TabIndex = 8;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(352, 120);
+            this.txt_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(212, 23);
+            this.txt_name.TabIndex = 7;
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(370, 212);
+            this.btn_ok.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(194, 34);
+            this.btn_ok.TabIndex = 6;
+            this.btn_ok.Text = "QR Kod Oluştur";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 292);
+            this.Controls.Add(this.pict);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_hes);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.btn_ok);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HesCodeQR";
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pict;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_hes;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ HesCodeQr/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing.QrCode;
+using ZXing;
+using ZXing.Common;
+using System.Drawing.Printing;
+
+namespace HesCodeQr
+{
+    public partial class Form1 : Form
+    {
+        private QrCodeEncodingOptions options;
+
+        public Form1()
+        {
+            InitializeComponent();
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250,
+            };
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txt_hes.Text) || String.IsNullOrEmpty(txt_hes.Text))
+            {
+                pict.Image = null;
+                MessageBox.Show("", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var qr = new ZXing.BarcodeWriter();
+                qr.Options = options;
+                qr.Format = ZXing.BarcodeFormat.QR_CODE;
+                var result = new Bitmap(qr.Write("c64dc337b87b41d08a165ffb020126ac|"+txt_hes.Text.Trim()));
+                pict.Image = result;
+                printPreviewDialog1.Document = printDocument1;
+                printPreviewDialog1.ShowDialog();
+                //printDocument1.Print();
+                txt_name.Clear();
+                txt_hes.Clear();
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            int width = (e.PageBounds.Width / 4);
+            e.Graphics.DrawString(txt_name.Text, new Font("Arial", 20, FontStyle.Regular), Brushes.Black, new Point(45, 20));
+            e.Graphics.DrawImage(pict.Image,0,60, width, width);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
